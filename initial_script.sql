b8d2fc9b9dc399e30ebabaa94048d090847db981
chris <chris.ross@7digital.com>
2016-09-12 16:59:25 +0100

    Fix db connection

---
+++ initial_script.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE osl
+  WITH OWNER = postgres
+  TEMPLATE = template0
+  ENCODING = 'UTF8'
+  LC_COLLATE = 'C'
+  LC_CTYPE = 'C'
+  CONNECTION LIMIT = -1;
--- settings_local.py
+++ settings_local.py
@@ -1,2 +1,2 @@
-SQL_URL = 'postgresql://localhost/test'
+SQL_URL = 'postgresql://chrisr:chrisr@postgres/chrisr'
 #APP_HOST = '127.0.0.1:5000'
